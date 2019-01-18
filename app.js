95306b5c5ff2b842825e5350646a912dd657239e
Yu-Jin Tan <yujin_1991@hotmail.com>
2019-01-18 16:47:04 +0000

    immediately invoke it

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 (function() {
     console.log('return');
-}) //function expression, surrounding function with parenthesis.
+}()) //function expression, surrounding function with parenthesis.
 
 console.log('return');
